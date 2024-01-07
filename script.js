58ea3c9dda43c9856638f165909fb35ad446cf56
L1TnzO <thenzo22@gmail.com>
2024-01-07 12:32:14 -0300

    implements operations methods

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 const btnContainer = document.querySelector('.buttons-container')
 
-btnContainer.addEventListener ('click', (e)=> {
-    alert(e.target.textContent);
+const history = document.querySelector('.history');
+
+btnContainer.addEventListener ('click', (event)=> {
+    let target = event.target;
+    //Check if the element is a direct child div of the container
+    if (target.matches('.buttons-container > div')) {
+        switch (target.textContent){
+            case '=':
+                alert('equal');
+                break
+            case '⌫':
+                history.textContent = history.textContent.slice(0, -1);
+                break
+            case 'Ac':
+                history.textContent = '';
+                break
+            case '/':
+                operate()
+
+            default:
+                history.textContent += target.textContent;
+        }
+    }
 })
+
+const operate = () => {
+    let str = history.textContent;
+    if (str.includes('/') || str.includes('*') || str.includes('-') || str.includes('+')) {
+        equal(str)
+        
+    }
+}
+
+const equal = (str)=>{
+    const foundOperator = str.match(/[/*+-]/)[0];
+    alert(foundOperator)
+    alert(str.split(foundOperator));
+    [a,b] = [str.split(foundOperator)[0],str.split(foundOperator)[1]]
+    switch (foundOperator){
+        case '/':
+            alert(a/b);
+            break
+        case '*':
+            alert(a*b);
+            break
+        case '-':
+            alert(a-b);
+            break
+        case '+':
+            alert(a+b);
+            break
+    }
+}
