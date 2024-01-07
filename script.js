b1e88beffe505a4d7727a1677452147ed2090e7f
L1TnzO <thenzo22@gmail.com>
2024-01-07 13:57:20 -0300

    fix operation when presses twice an operator

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ const btnContainer = document.querySelector('.buttons-container')
 
 const history = document.querySelector('.history');
 
+const result = document.querySelector('.result')
+
 btnContainer.addEventListener ('click', (event)=> {
     let target = event.target;
     //Check if the element is a direct child div of the container
     if (target.matches('.buttons-container > div')) {
         switch (target.textContent){
             case '=':
-                alert('equal');
-                break
+                equal(history.textContent);
+                break;
             case '⌫':
                 history.textContent = history.textContent.slice(0, -1);
-                break
+                break;
             case 'Ac':
                 history.textContent = '';
-                break
+                result.textContent = '0';
+                break;
             case '/':
-                operate()
-
+            case '*':
+            case '-':
+            case '+':
+                operate();
+                history.textContent += target.textContent;
+                break;
             default:
                 history.textContent += target.textContent;
         }
@@ -27,29 +34,34 @@ btnContainer.addEventListener ('click', (event)=> {
 
 const operate = () => {
     let str = history.textContent;
+    str = str.replace(/([/*+-]).*?([/*+-])/, '');
     if (str.includes('/') || str.includes('*') || str.includes('-') || str.includes('+')) {
         equal(str)
-        
+        history.textContent = result.textContent;
     }
 }
 
+
 const equal = (str)=>{
     const foundOperator = str.match(/[/*+-]/)[0];
-    alert(foundOperator)
-    alert(str.split(foundOperator));
-    [a,b] = [str.split(foundOperator)[0],str.split(foundOperator)[1]]
-    switch (foundOperator){
-        case '/':
-            alert(a/b);
-            break
-        case '*':
-            alert(a*b);
-            break
-        case '-':
-            alert(a-b);
-            break
-        case '+':
-            alert(a+b);
-            break
+    [a,b] = str.split(foundOperator).map(x => x ? Number(x) : NaN); //Verify if the second parameter is empty, and converts it to NaN
+    if (!isNaN(b)) {
+        switch (foundOperator){
+            case '/':
+                resultNumber = a/b;
+                break
+            case '*':
+                resultNumber = a*b;
+                break
+            case '-':
+                resultNumber = a-b;
+                break
+            case '+':
+                resultNumber = a+b;
+                break
+        }
+        result.textContent = resultNumber.toFixed(8).replace(/\.?0+$/, '');
     }
 }
+
+//TODO: fix minus operation.
